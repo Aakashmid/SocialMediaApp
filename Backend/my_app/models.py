4b2150f2ddd35bef1bf0d6fb62cb4374a09f3d35
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-08-03 11:39:28 +0530

    uploading code

--- Backend/my_app/models.py
+++ Backend/my_app/models.py
@@ -1,18 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Profile(AbstractUser):
-    bio=models.CharField(blank=True)
-    followers=models.ManyToManyField('self',related_name='followers',symmetrical=False)                        
-    # followings=models.ManyToManyField('self',related_name='followers',symmetrical=False)                        
-    profileImg=models.ImageField(upload_to='profile_images',blank=True ,null=True)
+class Profile(models.Model):
+    user=models.OneToOneField(User,related_name='profile', on_delete=models.CASCADE)
+    bio=models.CharField(blank=True,max_length=200)
+    profileImg=models.ImageField(upload_to='profile_images',blank=True)
     
+    def __str__(self) -> str:
+        return self.user.username
 
 class Post(models.Model):
-    pass
+    author=models.ForeignKey(Profile,related_name='posts',on_delete=models.CASCADE)  # related name help to query related date 
+    text=models.CharField( max_length=500)
+    postImg=models.ImageField(upload_to='post_images',blank=True,null=True)
+    publish_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.text[:50]+' ...'
 
 class Comment(models.Model):
-    pass
+    user=models.ForeignKey(Profile,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,related_name='comments',on_delete=models.CASCADE)
+    parent=models.ForeignKey('self',on_delete=models.CASCADE,null=True)
 
+
+
+class Follower(models.Model):
+    Author=models.ForeignKey(Profile,related_name='followers',on_delete=models.CASCADE)
+    follower=models.ForeignKey(Profile,related_name='followings' ,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return 'Author - '+ self.Author +', follower -'+self.follower    
+
+class Like(models.Model):
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,related_name='likes', on_delete=models.CASCADE,null=True)
+    Comment=models.ForeignKey(Comment,related_name='likes', on_delete=models.CASCADE,null=True)
+    created_at=models.DateTimeField( auto_now_add=True)
